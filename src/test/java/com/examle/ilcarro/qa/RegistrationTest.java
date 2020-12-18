9f2f98993a59544a3875f35ebf13984acaa867a5
Lena <etarnovskaya@gmail.com>
2020-12-18 09:27:08 +0200

    Created RegistrationTest, in Progress

---
+++ src/test/java/com/examle/ilcarro/qa/RegistrationTest.java
@@ -0,0 +1,32 @@
+package com.examle.ilcarro.qa;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RegistrationTest {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.get("https://google.com");
+    }
+
+    @Test
+    public void testRegistration(){
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+
+    }
+
+}
+
+
