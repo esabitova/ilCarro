aaca9362c7c5ce22c5517db9ee8c69fdf0bfad35
Lena <etarnovskaya@gmail.com>
2021-01-08 12:00:16 +0200

    added system proprties 'browser'

--- src/test/java/com/examle/ilcarro/qa/application/ApplicationManager.java
+++ src/test/java/com/examle/ilcarro/qa/application/ApplicationManager.java
@@ -2,25 +2,40 @@ package com.examle.ilcarro.qa.application;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-        WebDriver wd;
-        UserHelper userHelper;
-        CarHelper carHelper;
+    WebDriver wd;
+    UserHelper userHelper;
+    CarHelper carHelper;
+    String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void start() {
-        wd = new ChromeDriver();
+        if(browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        }else if(browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        }else if(browser.equals(BrowserType.EDGE)){
+            wd = new EdgeDriver();
+        }
+
         wd.navigate().to("https://ilcarro-dev-v1.firebaseapp.com/");
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
-       userHelper = new UserHelper(wd);
-       carHelper = new CarHelper(wd);
-   }
+        userHelper = new UserHelper(wd);
+        carHelper = new CarHelper(wd);
+    }
 
     public void stop() {
         wd.quit();
--- src/test/java/com/examle/ilcarro/qa/tests/TestBase.java
+++ src/test/java/com/examle/ilcarro/qa/tests/TestBase.java
@@ -1,11 +1,13 @@
 package com.examle.ilcarro.qa.tests;
 
 import com.examle.ilcarro.qa.application.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.*;
 
 public class TestBase {
 
-    protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp(){
